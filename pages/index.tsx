0f684ea37710398d84f0cc17fc6a56568e4a7a0f
ssamkkim <ssamuel.kkim@gmail.com>
2023-01-09 16:50:03 -0800

    completed module 1.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,44 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
-import * as Web3 from '@solana/web3.js'
+import * as Web3 from '@solana/web3.js';
+import { connect } from 'http2';
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import AddressForm from '../components/AddressForm';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState('');
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
-      setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
-      connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
-      })
+      setAddress(address);
+      const key = new Web3.PublicKey(address);
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
+      connection.getBalance(key).then((balance) => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL);
+      });
+      connection.getAccountInfo(key).then((info) => {
+        setIsExecutable(info?.executable ?? false);
+      });
     } catch (error) {
-      setAddress('')
-      setBalance(0)
-      alert(error)
+      setAddress('');
+      setBalance(0);
+      alert(error);
     }
-  }
+  };
 
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <p>
-          Start Your Solana Journey
-        </p>
+        <p>Start Your Solana Journey</p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
